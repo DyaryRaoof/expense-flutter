5f751161491e2141f26a1de55931116b9d71b597
Dyary Raoof <dyray2020@gmail. om>
2022-05-16 21:12:59 +0300

    Some improvements

--- lib/widgets/new-transaction.dart
+++ lib/widgets/new-transaction.dart
@@ -46,47 +46,52 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          TextField(
-            decoration: const InputDecoration(hintText: 'Title'),
-            controller: _titleController,
-            onSubmitted: (_) => submitData(),
-          ),
-          TextField(
-            decoration: const InputDecoration(hintText: 'Price'),
-            controller: _priceController,
-            onSubmitted: (_) => submitData(),
-            keyboardType: TextInputType.number,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              TextButton(
-                onPressed: _presentDatePicker,
-                child: Text(
-                  _date == null
-                      ? 'No Date Chosen'
-                      : DateFormat.yMd().format(_date!).toString(),
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+    return SingleChildScrollView(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Column(
+            children: [
+              TextField(
+                decoration: const InputDecoration(hintText: 'Title'),
+                controller: _titleController,
+                onSubmitted: (_) => submitData(),
               ),
-              TextButton(
-                onPressed: _presentDatePicker,
-                child: const Text(
-                  'Select Date',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+               TextField(
+                decoration: const InputDecoration(hintText: 'Price'),
+                controller: _priceController,
+                onSubmitted: (_) => submitData(),
+                keyboardType: TextInputType.number,
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: _presentDatePicker,
+                    child: Text(
+                      _date == null
+                          ? 'No Date Chosen'
+                          : DateFormat.yMd().format(_date!).toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: _presentDatePicker,
+                    child: const Text(
+                      'Select Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: TextButton(
+                    onPressed: submitData, child: const Text('Add Transaction')),
+              )
             ],
           ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: TextButton(
-                onPressed: submitData, child: const Text('Add Transaction')),
-          )
-        ],
+        ),
       ),
     );
   }
